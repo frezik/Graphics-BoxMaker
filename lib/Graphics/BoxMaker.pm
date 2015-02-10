7c7ee80e22d06864947005de5e26319a4a943aff
tmurray <tmurray@wumpus-cave.net>
2015-02-10 15:56:08 -0600

    Setup basic attributes

--- lib/Graphics/BoxMaker.pm
+++ lib/Graphics/BoxMaker.pm
@@ -3,9 +3,23 @@ package Graphics::BoxMaker;
 use v5.12;
 use warnings;
 use Moose;
+use Moose::Util::TypeConstraints;
 use namespace::autoclean;
 
 # ABSTRACT: Create boxes for CNC machines, such as laser cutters
+
+
+has $_ => (
+    is       => 'ro',
+    isa      => 'Num',
+    required => 1,
+) for (qw{ width_mm height_mm depth_mm thickness_mm kerf });
+
+has 'joins' => (
+    is      => 'ro',
+    isa     => enum([qw{ simple finger }]),
+    default => 'simple',
+);
 
 
 no Moose;
