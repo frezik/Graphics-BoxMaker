e13369f9658e9bdd4eaffd17e49b98a24dca70be
tmurray <tmurray@wumpus-cave.net>
2015-02-10 15:29:42 -0600

    Initial import

---
+++ lib/Graphics/BoxMaker.pm
@@ -0,0 +1,14 @@
+package Graphics::BoxMaker;
+
+use v5.12;
+use warnings;
+use Moose;
+use namespace::autoclean;
+
+# ABSTRACT: Create boxes for CNC machines, such as laser cutters
+
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+1;
+__END__
